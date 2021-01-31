0aee0c319ef2d93e111d6b9cfb0544825a268402
Stacey <yecats131@gmail.com>
2021-01-31 15:11:51 -0800

    Building out the demo

---
+++ tutorials/Unity/Create-an-Inventory-UI-with UI-Toolkit/projects/finalProject/InventoryUI/Assets/WUG/Scripts/InventorySlot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine.UIElements;
+
+namespace Assets.WUG.Scripts
+{
+    public class InventorySlot : VisualElement
+    {
+        public Image Icon;
+        public string ItemGuid = "";
+
+        public InventorySlot()
+        {
+            //Create a new Image element and add it to the root
+            Icon = new Image();
+            Add(Icon);
+
+            //Add USS style properties to the elements
+            Icon.AddToClassList("slotIcon");
+            AddToClassList("slotContainer");
+        }
+    }
+}
---
+++ tutorials/Unity/Create-an-Inventory-UI-with UI-Toolkit/projects/finalProject/InventoryUI/Assets/WUG/Scripts/InventoryUIController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine.UIElements;
+using UnityEngine;
+
+namespace Assets.WUG.Scripts
+{
+    public class InventoryUIController : MonoBehaviour
+    {
+        public List<InventorySlot> InventoryItems = new List<InventorySlot>();
+
+        private VisualElement m_Root;
+        private VisualElement m_SlotContainer;
+
+        private void Start()
+        {
+            //Store the root from the UI Document component
+            m_Root = GetComponent<UIDocument>().rootVisualElement;
+
+            //Search the root for the SlotContainer Visual Element
+            m_SlotContainer = m_Root.Query<VisualElement>("SlotContainer");
+
+            //Create 20 InventorySlots and add them as children to the SlotContainer
+            for (int i = 0; i < 20; i++)
+            {
+                InventorySlot item = new InventorySlot();
+
+                InventoryItems.Add(item);
+
+                m_SlotContainer.Add(item);
+            }
+
+        }
+
+    }
+}
