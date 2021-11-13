677da2eec6f7893d813a9dff935d1d39044c2e88
Stacey <4108756+Yecats@users.noreply.github.com>
2021-11-12 19:24:33 -0800

    New Tutorial:  Create a compass with uGUI (#21)

---
+++ tutorials/Unity/create-a-compass-with-ugui/finalProject/Assets/Scripts/CompassManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.WUG.Scripts
+{
+    public class CompassManager : MonoBehaviour
+    {
+        public static CompassManager Instance;
+
+        public RawImage CompassImage;
+        public RectTransform CompassObjectivesParent;
+        public GameObject CompassObjectivePrefab;
+        private readonly List<CompassObjective> _currentObjectives = new List<CompassObjective>();
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private IEnumerator Start()
+        {
+            WaitForSeconds updateDelay = new WaitForSeconds(1);
+
+            while (enabled)
+            {
+                SortCompassObjectives();
+                yield return updateDelay;
+            }
+        }
+
+        private void SortCompassObjectives()
+        {
+            if (PlayerController.Instance == null)
+            { return; }
+
+            CompassObjective[] orderedObjectives = _currentObjectives.Where(o => o.WorldGameObject != null).OrderByDescending(o => Vector3.Distance(PlayerController.Instance.transform.position, o.WorldGameObject.position)).ToArray();
+            for (int i = 0; i < orderedObjectives.Length; i++)
+            {
+                orderedObjectives[i].UpdateUiIndex(i);
+            }
+        }
+
+        /// <summary>
+        /// Called at end of frame -> update compass orientation and all objective positions
+        /// </summary>
+        private void LateUpdate() => UpdateCompassHeading();
+
+        /// <summary>
+        /// Updates the orientation of the compass heading image
+        /// </summary>
+        private void UpdateCompassHeading()
+        {
+            if (PlayerController.Instance == null)
+            { return; }
+
+            //get compass offset position
+            Vector2 compassUvPosition = Vector2.right * (PlayerController.Instance.transform.rotation.eulerAngles.y / 360);
+
+            //set if flipped, interpolate if not
+            CompassImage.uvRect = new Rect(compassUvPosition, Vector2.one);
+        }
+
+        /// <summary>
+        /// Call with game object to create compass objective
+        /// </summary>
+        public void AddObjectiveForObject(GameObject compassObjectiveGameObject, Color color, Sprite sprite) =>
+            _currentObjectives.Add(Instantiate(CompassObjectivePrefab, CompassObjectivesParent, false)
+                .GetComponent<CompassObjective>().Configure(compassObjectiveGameObject, color, sprite));
+
+        public void RemoveCompassObjective(CompassObjective compassObjective)
+        {
+            _currentObjectives.Remove(compassObjective);
+            Destroy(compassObjective.gameObject);
+        }
+    }
+}
---
+++ tutorials/Unity/create-a-compass-with-ugui/finalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+
+/// <summary>
+/// For example only
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+
+    private Rigidbody _rigidBody;
+
+    private const float MoveSpeed = 5;
+    private const float RotationSpeed = MoveSpeed / 2;
+    private const string MouseInputAxis = "Mouse X";
+    [SerializeField]
+    private GameObject _bowModel;
+    [SerializeField]
+    private GameObject _quiverModel;
+
+    private static Animator _animator;
+
+    public bool IsGrounded => Physics.Raycast(transform.position, Vector3.down, 1.1f, 1 << 6);
+    public void ShowBow() => _bowModel.gameObject.SetActive(true);
+    public void ShowQuiver() => _quiverModel.gameObject.SetActive(true);
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() 
+    { 
+        _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    //update rotation
+    private void Update() => _rigidBody.MoveRotation(_rigidBody.rotation * Quaternion.AngleAxis(Input.GetMouseButton(1) ? Input.GetAxis(MouseInputAxis) * RotationSpeed : 0, Vector3.up));
+
+    //update position
+    private void FixedUpdate()
+    {
+        Vector3 newDir = GetInputDirection();
+
+        _rigidBody.MovePosition(_rigidBody.position + (transform.rotation * newDir * Time.fixedDeltaTime * MoveSpeed));
+
+        if (newDir == Vector3.zero && IsGrounded)
+        {
+            _rigidBody.velocity = Vector3.zero;
+            _rigidBody.angularVelocity = Vector3.zero;
+        }
+
+    }
+
+    //returns vector of player input
+    private static Vector3 GetInputDirection()
+    {
+        Vector3 nextMovement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            nextMovement += Vector3.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            nextMovement += Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            nextMovement += Vector3.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            nextMovement += Vector3.right;
+        }
+
+        if (nextMovement == Vector3.zero && _animator.GetBool("Run"))
+        {
+            _animator.SetBool("Run", false);
+        }
+        else if (nextMovement != Vector3.zero && !_animator.GetBool("Run"))
+        { 
+            _animator.SetBool("Run", true);
+        }
+
+
+        return nextMovement;
+    }
+}
