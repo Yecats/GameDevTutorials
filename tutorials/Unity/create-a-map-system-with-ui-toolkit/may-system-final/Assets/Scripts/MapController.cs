d64199a1c41c9aad7b7f420c2636a0ba7bc069f0
Stacey <yecats131@gmail.com>
2021-11-27 15:46:54 -0800

    fixed the multiplyer so that the map moves properly

--- tutorials/Unity/create-a-map-system-with-ui-toolkit/may-system-final/Assets/Scripts/MapController.cs
+++ tutorials/Unity/create-a-map-system-with-ui-toolkit/may-system-final/Assets/Scripts/MapController.cs
@@ -10,7 +10,9 @@ public class MapController : MonoBehaviour
     private VisualElement _mapImage;
 
     [Range(1, 15)]
-    public float Multiplyer = 1f;
+    public float miniMultiplyer = 1f;
+    [Range(1, 15)]
+    public float fullMultiplyer = 1f;
     private bool _mapFaded;
     public bool MapFaded
     {
@@ -45,6 +47,7 @@ public class MapController : MonoBehaviour
         _playerRepresentation = _root.Q<VisualElement>("Player");
         _mapImage = _root.Q<VisualElement>("Image");
         _mapContainer = _root.Q<VisualElement>("Map");
+
     }
 
     void LateUpdate()
@@ -56,7 +59,8 @@ public class MapController : MonoBehaviour
         }
 
         //Rotate and move the player icon based on the players movement
-        _playerRepresentation.style.translate = new Translate(Player.transform.position.x * Multiplyer, Player.transform.position.z * -Multiplyer, 0);
+        var multiplyer = IsMapOpen ? fullMultiplyer : miniMultiplyer;
+        _playerRepresentation.style.translate = new Translate(Player.transform.position.x * multiplyer, Player.transform.position.z * -multiplyer, 0);
         _playerRepresentation.style.rotate = new Rotate(new Angle(Player.transform.rotation.eulerAngles.y));
 
         //Alter the faded value if the map is open and the player is moving
@@ -70,8 +74,8 @@ public class MapController : MonoBehaviour
             var clampHeight = _mapImage.worldBound.height / 2 - _mapContainer.worldBound.height / 2;
 
             //Clamp the bounds so that the map doesn't scroll past the playable area (i.e. the map image)
-            var xPos = Mathf.Clamp(Player.transform.position.x * -Multiplyer, -clampWidth, clampWidth);
-            var yPos = Mathf.Clamp(Player.transform.position.z * Multiplyer, -clampHeight, clampHeight);
+            var xPos = Mathf.Clamp(Player.transform.position.x * -miniMultiplyer, -clampWidth, clampWidth);
+            var yPos = Mathf.Clamp(Player.transform.position.z * miniMultiplyer, -clampHeight, clampHeight);
 
             //Move the map image
             _mapImage.style.translate = new Translate(xPos, yPos, 0);
