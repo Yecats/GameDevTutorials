7f728b00c865fb1bdbeafb316e665351f89e09b7
Stacey <yecats131@gmail.com>
2022-04-03 10:36:14 -0700

    Working on transition

--- tutorials/Unity/ui-toolkit-main-menu-anim/UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/Temp.cs
+++ tutorials/Unity/ui-toolkit-main-menu-anim/UI-Toolkit-Main-Menu-Final/Assets/WUG/Scripts/Temp.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Temp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private VisualElement _root;
+    private VisualElement _menu;
+
+    private void Start()
     {
-        
+        _root = GetComponent<UIDocument>().rootVisualElement;
+        _menu = _root.Q<VisualElement>("Menu");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    [ContextMenu("Animate")]
+    public void AnimateTest()
     {
-        
+        _menu.style.left = new StyleLength(-200f);
     }
 }
